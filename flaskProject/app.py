341175f5113113151845d789c4e17c68f75b3ff1
BrianCosta2004 <brianhcosta@gmail.com>
2022-11-15 02:40:37 +0000

    New Pages
    Signed-off-by: BrianCosta2004 <brianhcosta@gmail.com>

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lista_usuarios = ['Brian', 'Costa', 'Teste']
+
+app.config['SECRET_KEY'] = 'dcc92759b404cc3d1adf6e83265a0d1e'
+
 
 @app.route('/')
-def hello_world():  
-    return 'Hello World!'
+def home():
+    return render_template('homepage.html')
+
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
---
+++ flaskProject/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6,20)])
+    confirmacao = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criarconta = SubmitField('Criar Conta')
+
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    botao_submit_login = SubmitField('Login')
